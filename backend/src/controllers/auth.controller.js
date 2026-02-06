c2ff0eb7b9ce5cbd97d62b34b7faaef527cbdfbf
Nehalshaikh8698 <nehalshaikh02017@gmail.com>
2026-02-07 01:11:48 +0530

    day-2 compeleted all the authentication related backend like signup,login,logout,check

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import cloudnary from "../lib/cloudnary.js";
 export const signup =async (req,res) =>{
     const { fullName, email, password } = req.body;
     try {
@@ -86,5 +87,37 @@ export const logout = (req,res) =>{
         console.log("Error in logout controller",error.message);
         res.status(500).json({message:"Internal Server Error"});
         
+    }
+}
+
+export const updateProfile = async (req,res)=>{
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message:"Profile Pic is required"});
+
+        }
+       const uploadResponse = await cloudnary.uploader.upload(profilePic);
+       const updatedUser = await User.findByIdAndUpdate(
+        {profilePic:uploadResponse.secure_url},
+        {new : true}
+       );
+
+       res.status(200).json(updatedUser);
+
+    } catch (error) {
+        console.log("Error in update profile:",error.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+};
+
+export const checkAuth = (req,res) =>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth ,",error.message);
+        res.status(500).json({message:"Internal Server Error"});
     }
 }

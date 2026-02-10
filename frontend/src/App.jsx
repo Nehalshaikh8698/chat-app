3b4f5251ebb5d6d85c9aa3f0be6791e795257b25
Nehalshaikh8698 <nehalshaikh02017@gmail.com>
2026-02-10 16:08:42 +0530

    day3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
+import Navbar from './components/Navbar'
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import SettingPage from "./pages/SettingPage"
+import ProfilePage from "./pages/ProfilePage"
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className=" text-amber-900">
-      
+    <div data-theme="coffee">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+
     </div>
   )
 }
